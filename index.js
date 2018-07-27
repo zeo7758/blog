79d45dd45c41eaabee231d70323c640c48bd9dde
zeo7758 <17621063136@163.com>
2018-07-27 15:12:41 +0800

    view ejs

--- index.js
+++ index.js
@@ -11,4 +11,50 @@ const config = require('./config/default');
 
 const app = new Koa();
 
+// session存储配置
+// const sessionMysqlConfig= {
+//   user: config.database.USERNAME,
+//   password: config.database.PASSWORD,
+//   database: config.database.DATABASE,
+//   host: config.database.HOST,
+// }
+
+// 配置session中间件
+// app.use(session({
+//   key: 'USER_SID',
+//   store: new MysqlStore(sessionMysqlConfig)
+// }))
+
+
+// 配置静态资源加载中间件
+// app.use(koaStatic(
+//   path.join(__dirname , './public')
+// ))
+// 缓存
+// app.use(staticCache(path.join(__dirname, './public'), { dynamic: true }, {
+//   maxAge: 365 * 24 * 60 * 60
+// }))
+// app.use(staticCache(path.join(__dirname, './images'), { dynamic: true }, {
+//   maxAge: 365 * 24 * 60 * 60
+// }))
+
+
+// 配置服务端模板渲染引擎中间件
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}))
+app.use(bodyParser({
+  formLimit: '1mb'
+}))
+
+// 路由
+//app.use(require('./routers/signin.js').routes())
+app.use(require('./routers/signUp.js').routes())
+//app.use(require('./routers/posts.js').routes())
+//app.use(require('./routers/signout.js').routes())
+
+
+
+
+
 app.listen('8888')
