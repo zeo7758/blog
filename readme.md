f7747a859319e3fb4245fd9ca357b59e0fb8164b
zeo7758 <17621063136@163.com>
2018-08-01 16:25:14 +0800

    config change

--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
-## readme
+# my-blog --个人博客
+
+> vue + koa + mongodb
+
+## Build Setup
+
+<!-- ``` bash
+# install dependencies222
+npm install
+#test
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+``` -->
--- web/config/index.js
+++ web/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
@@ -41,7 +41,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
